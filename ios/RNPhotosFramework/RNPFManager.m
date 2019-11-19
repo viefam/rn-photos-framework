2ae13a94e839730d5aad1076f9c02332167ba30d
Flash <vasile.taranovici@gmail.com>
2019-11-19 15:06:15 +0200

    Update RNPFManager.m

--- ios/RNPhotosFramework/RNPFManager.m
+++ ios/RNPhotosFramework/RNPFManager.m
@@ -1,5 +1,5 @@
 #import <React/RCTConvert.h>
-#import <React/RCTImageLoader.h>
+#import <React/RCTImageLoaderProtocol.h>
 #import <React/RCTLog.h>
 #import <React/RCTUtils.h>
 #import <React/RCTProfile.h>
@@ -638,7 +638,7 @@ andProgressBlock:(fileDownloadProgressBlock)progressBlock {
         [self saveVideo:request.source toAlbum:collectionLocalIdentifier andCompleteBLock:completeBlock andProgressBlock:progressBlock];
     } else if([request.type isEqualToString:@"image"]) {
         NSURLRequest *url = [RCTConvert NSURLRequest:request.source.uri];
-        [[_bridge moduleForClass:[RCTImageLoader class]] loadImageWithURLRequest:url
+        [[self.bridge moduleForName:@"ImageLoader" lazilyLoadIfNecessary:YES] loadImageWithURLRequest:url
                                                     size:CGSizeZero
                                                    scale:1
                                                  clipped:YES
@@ -905,7 +905,7 @@ RCT_EXPORT_METHOD(removeAssetsFromAlbum:(NSDictionary *)params
     NSURLComponents *components = [[NSURLComponents alloc] initWithURL:imageURLRequest.URL resolvingAgainstBaseURL:NO];
     components.queryItems = [self parseParamsToImageLoaderQueryOptions:params];
     
-    return [[_bridge moduleForClass:[RCTImageLoader class]] loadImageWithURLRequest:[NSURLRequest requestWithURL:components.URL]
+    return [[self.bridge moduleForName:@"ImageLoader" lazilyLoadIfNecessary:YES] loadImageWithURLRequest:[NSURLRequest requestWithURL:components.URL]
                                                 size:size
                                                scale:scale
                                              clipped:clipped
